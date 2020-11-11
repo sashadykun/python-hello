46586168ab932e892639a490035c59843ca128d1
SD <sashaluckyman@gmail.com>
2020-11-10 21:50:42 -0800

    add session concept to see different content base on session

--- tasks/views.py
+++ tasks/views.py
@@ -12,8 +12,10 @@ class NewTaskForm(forms.Form):
 # Create your views here.
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
@@ -21,7 +23,9 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            # tasks.append(task)
+            request.session["tasks"] += [task]
+            
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "tasks/add.html", {
